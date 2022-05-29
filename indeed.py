c99bebc4da5d480f11f6cc3952b480871bd45fc0
cju0727 <chojw0727@gmail.com>
2022-05-29 22:15:28 +0900

    Update main.py, indeed.py(extract_indeed_jobs)

--- indeed.py
+++ indeed.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-INDEED_URL = "https://kr.indeed.com/jobs?q=python&limit=50"
+LIMIT = 50
+URL = f"https://kr.indeed.com/jobs?q=python&limit={LIMIT}"
 
 def extract_indeed_pages():
-  result = requests.get(INDEED_URL)
+  result = requests.get(URL)
 
   soup = BeautifulSoup(result.text, "html.parser")
 
@@ -20,3 +21,10 @@ def extract_indeed_pages():
   max_page = pages[-1]
 
   return max_page
+
+
+def extract_indeed_jobs(last_page):
+  for page in range(last_page):
+    result = requests.get(f"{URL}&start={page*LIMIT}")
+
+    print(result.status_code)
