bcef9e55f1aea0c9134c99eb4967d35eec1d9e66
cju0727 <chojw0727@gmail.com>
2022-06-10 18:37:54 +0900

    Finish indeed.py

--- indeed.py
+++ indeed.py
@@ -24,22 +24,32 @@ def extract_indeed_pages():
 
     return max_page
 
+def extract_job(html):
+    title = html.find("div", {"class": "heading4"}).find("span", title=True).string
+    company = html.find("span", {"class": "companyName"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    location = html.find("div", {"class": "companyLocation"}).string
+    link_id = html.find("a")["data-jk"]
+    return {
+      "title": title, 
+      "company": company, 
+      "location": location, 
+      "link": f"https://kr.indeed.com/viewjob?jk={link_id}"}
+
 
 def extract_indeed_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class": "job_seen_beacon"})
-    for result in results:
-        title = result.find("div", {"class": "heading4"}).find("span", title=True).string
-        company = result.find("span", {"class": "companyName"})
-        company_anchor = company.find("a")
-        if company_anchor is not None:
-          company = str(company_anchor.string)
-        else:
-          company = str(company.string)
-        print("모집분야, 직무 : ", title, "\n회사명 : " ,company, "\n")
-
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "job_seen_beacon"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+            
     return jobs
-    # print(result.status_code)
